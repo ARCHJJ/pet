58f1411dbb20e5eae560364d2d5548507d26cf3a
박지섭 <qhtlr4@naver.com>
2017-09-15 15:08:55 +0900

    properties 수정, market/itemdao 추가

---
+++ src/com/action/search/ItemSearchAction.java
@@ -0,0 +1,27 @@
+package com.action.search;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.action.CommonAction;
+import com.model.board.BoardBean;
+import com.model.mymenu.market.ItemBean;
+import com.model.mymenu.market.ItemDao;
+
+public class ItemSearchAction extends CommonAction {
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		// TODO Auto-generated method stub
+		
+		String word = request.getParameter("word");
+
+		ArrayList<ItemBean> iblist = new ArrayList<ItemBean>();
+		iblist=ItemDao.getInstance().searchItemList(word);
+		
+		request.getSession().setAttribute("iblist", iblist);
+		
+		return "view/search/itemSearch.jsp";
+	}
+
+}
