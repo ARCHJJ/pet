239354a8ecea40888ad8332096ace2f3b9eeaac6
dhukim <dhukim@naver.com>
2017-11-10 17:03:08 +0900

    사진 업로드 수정
    사진 추가 업로드 없을때 사진에 null 들어가는것 수정

--- src/com/action/mymenu/market/Market_Menu_Info_Modify_Action.java
+++ src/com/action/mymenu/market/Market_Menu_Info_Modify_Action.java
@@ -69,8 +69,12 @@ public class Market_Menu_Info_Modify_Action extends CommonAction {
 					}
 				}
 			}
-
+			if(name != null)
 			updateMarket(market_id, market_name, description, name, address, check[0], check[1], check[2], check[3], check[4],
+					check[5], check[6], check[7], check[8], check[9], check[10], check[11], check[12], check[13],
+					check[14]);
+			else
+			updateMarket(market_id, market_name, description, address, check[0], check[1], check[2], check[3], check[4],
 					check[5], check[6], check[7], check[8], check[9], check[10], check[11], check[12], check[13],
 					check[14]);
 
@@ -97,7 +101,16 @@ public class Market_Menu_Info_Modify_Action extends CommonAction {
 		return "mymenu_shop_info.do";
 	}
 
-	private void updateMarket(int market_id, String market_name, String description,String photos, String address, int cl_market,
+	private void updateMarket(int market_id, String market_name, String description, String address, int cl_market,
+			int cl_beauty, int cl_hotel, int cl_hospital, int ser_buypet, int ser_buystuff, int ser_hair, int ser_bath,
+			int ser_rent, int ser_lodge, int ser_doctor, int ser_surgery, int pet_dog, int pet_cat, int pet_etc) {
+		// TODO Auto-generated method stub
+		MarketDao.getInstance().updateMarket(market_id, market_name, description, address, cl_market, cl_beauty,
+				cl_hotel, cl_hospital, ser_buypet, ser_buystuff, ser_hair, ser_bath, ser_rent, ser_lodge, ser_doctor,
+				ser_surgery, pet_dog, pet_cat, pet_etc);
+	}
+
+	private void updateMarket(int market_id, String market_name, String description, String photos, String address, int cl_market,
 			int cl_beauty, int cl_hotel, int cl_hospital, int ser_buypet, int ser_buystuff, int ser_hair, int ser_bath,
 			int ser_rent, int ser_lodge, int ser_doctor, int ser_surgery, int pet_dog, int pet_cat, int pet_etc) {
 		// TODO Auto-generated method stub
--- src/com/model/mymenu/market/MarketDao.java
+++ src/com/model/mymenu/market/MarketDao.java
@@ -59,7 +59,7 @@ public class MarketDao extends DaoCore {
 			param.put("ser_doctor", ser_doctor);
 			param.put("ser_surgery", ser_surgery);
 
-			getSqlMapClient().update("MarketDao.updateMarket", param);
+			getSqlMapClient().update("MarketDao.updateMarket_a", param);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -74,5 +74,37 @@ public class MarketDao extends DaoCore {
 			e.printStackTrace();
 		}
 		return mflist;
+	}
+
+	public void updateMarket(int market_id, String market_name, String description, String address, int cl_market,
+			int cl_beauty, int cl_hotel, int cl_hospital, int ser_buypet, int ser_buystuff, int ser_hair, int ser_bath,
+			int ser_rent, int ser_lodge, int ser_doctor, int ser_surgery, int pet_dog, int pet_cat, int pet_etc) {
+		// TODO Auto-generated method stub
+		try {
+			HashMap<String, Object> param = new HashMap<String, Object>();
+			param.put("market_id", market_id);
+			param.put("market_name", market_name);
+			param.put("description", description);
+			param.put("address", address);
+			param.put("pet_dog", pet_dog);
+			param.put("pet_cat", pet_cat);
+			param.put("pet_etc", pet_etc);
+			param.put("cl_market", cl_market);
+			param.put("cl_beauty", cl_beauty);
+			param.put("cl_hotel", cl_hotel);
+			param.put("cl_hospital", cl_hospital);
+			param.put("ser_buypet", ser_buypet);
+			param.put("ser_buystuff", ser_buystuff);
+			param.put("ser_hair", ser_hair);
+			param.put("ser_bath", ser_bath);
+			param.put("ser_rent", ser_rent);
+			param.put("ser_lodge", ser_lodge);
+			param.put("ser_doctor", ser_doctor);
+			param.put("ser_surgery", ser_surgery);
+
+			getSqlMapClient().update("MarketDao.updateMarket_b", param);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
