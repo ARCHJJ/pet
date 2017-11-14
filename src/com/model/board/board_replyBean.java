228b44815d4b9927a0336162152359a2c78572bb
박지섭 <qhtlr4@naver.com>
2017-11-14 22:16:36 +0900

    Bean클래스 이름 변경으로 삭제

--- src/com/model/board/board_replyBean.java
+++ src/com/model/board/board_replyBean.java
@@ -2,36 +2,36 @@ package com.model.board;
 
 import java.sql.Date;
 
-public class board_replyBean {
-	private int parent_idx;
-	private int order;
+public class Board_replyBean {
+	private int board_id;
+	private int idx;
 	private String content;
-	private String writer;
+	private String email;
 	private Date date;
 	
-	public int getParent_idx() {
-		return parent_idx;
+	public int getBoard_id() {
+		return board_id;
 	}
-	public void setParent_idx(int parent_idx) {
-		this.parent_idx = parent_idx;
+	public void setBoard_id(int board_id) {
+		this.board_id = board_id;
 	}
-	public int getOrder() {
-		return order;
+	public int getIdx() {
+		return idx;
 	}
-	public void setOrder(int order) {
-		this.order = order;
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public String getContent() {
 		return content;
 	}
 	public void setContent(String content) {
 		this.content = content;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
 	}
 	public Date getDate() {
 		return date;
