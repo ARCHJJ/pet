1b73799c5113985aa70502d7f00300d85e706453
박지섭 <qhtlr4@naver.com>
2017-11-14 23:28:21 +0900

    매장 예약정보 삭제 수정

--- src/com/action/mymenu/market/Market_Menu_Reserv_Delete_Action.java
+++ src/com/action/mymenu/market/Market_Menu_Reserv_Delete_Action.java
@@ -9,6 +9,7 @@ import com.model.mymenu.user.ReservDao;
 public class Market_Menu_Reserv_Delete_Action extends CommonAction{
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		// TODO Auto-generated method stub
+		
 		String rev_idx = request.getParameter("rev_idx");
 
 		ReservDao.getInstance().deleteReservInfo(rev_idx);
